a5726a88c75c5e3037d873fc9e99742152e79e49
edwxrdbobb <sesayadamabella@gmail.com>
2024-12-23 15:32:59 +0000

    new ts ans eslint configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  plugins: [
+    react(),
+    eslint({
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      exclude: ['node_modules/**', 'dist/**'],
+      failOnError: false,
+    }),
+  ],
+  esbuild: {
+    jsxInject: `import React from 'react'`,
   },
-});
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    open: true,
+  },
+})
+
